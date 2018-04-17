b8882ae84c8bbe0a23ce62d6a7a298f4cc236828
ishan <ishan.office@gmail.com>
2018-04-17 16:02:36 +0530

    Commit for 17th Apr

--- truffle_back_end/truffle.js
+++ truffle_back_end/truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "oblige side ritual warfare symbol focus tomato exercise frost best curious movie";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,7 +10,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
-  }
+    },
+
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/cB2USDTPPAJ523I9LSxP")
+      },
+      gas: 6721975,
+      network_id: 3
+    }   
+  } 
 
 };
+
+
